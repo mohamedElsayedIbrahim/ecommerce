709d535929d7b8bed7e3029c3c59e9a6501708e4
mohamedElsayedIbrahim <mohamdeesayed@outlook.com>
2024-01-04 10:25:37 +0200

    deploying changing indexjs path

--- dist/js/index.js
+++
@@ -1,5 +0,0 @@
-import './modules/all.min.js'; //fontawsome
-import './modules/bootstrap.bundle.min.js'; //bootstrap
-
-import './website/about.js';
-import './website/product.js'
---
+++ index.js
@@ -0,0 +1,5 @@
+import './dist/js/modules/all.min.js'; //fontawsome
+import './dist/js/modules/bootstrap.bundle.min.js'; //bootstrap
+
+import './dist/js/website/about.js';
+import './dist/js/website/product.js'
